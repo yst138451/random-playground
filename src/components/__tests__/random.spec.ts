9b1d578fdfca1ac14126d1acedcb913eceb2e9c0
Yomanto T <yom@jibble.io>
2024-07-19 18:28:21 +0700

    fix test

--- src/components/__tests__/random.spec.ts
+++ src/components/__tests__/random.spec.ts
@@ -2,7 +2,7 @@ import  { describe, expect, test } from 'vitest'
 import  { sum } from '../random'
 
 describe('sum module', () => {
-  test('adds 1 + 2 to equal 3', () => {
-    expect(sum(1, 2)).toBe(3)
+  test('adds 1 + 2 + 1 to equal 4', () => {
+    expect(sum(1, 2)).toBe(4)
   })
 })
